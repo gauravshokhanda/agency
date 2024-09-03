4e63785dffe71b9e10abbb106f73067d2db1143a
Gaurav shokhanda <gauravshokanda2@gmail.com>
2024-09-03 13:10:54 +0530

    fix header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -158,8 +158,8 @@ const Header = () => {
                   </ul>
                 </nav>
               </div>
-              {/* <div className="flex items-center justify-end pr-16 lg:pr-0">
-                <Link
+              <div className="flex items-center justify-end pr-16 lg:pr-0">
+                {/* <Link
                   href="/signin"
                   className="hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70 dark:text-white md:block"
                 >
@@ -170,11 +170,11 @@ const Header = () => {
                   className="ease-in-up shadow-btn hover:shadow-btn-hover hidden rounded-sm bg-primary px-8 py-3 text-base font-medium text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6 xl:px-9"
                 >
                   Sign Up
-                </Link>
+                </Link> */}
                 <div>
                   <ThemeToggler />
                 </div>
-              </div> */}
+              </div>
             </div>
           </div>
         </div>
